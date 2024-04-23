ebcebde7bd5175072a9f9c76bc3eb33c2104a647
sanchyopas <saniagolovanev@gmail.com>
2024-04-23 18:17:23 +0700

    FIX

--- main/main/settings.py
+++ main/main/settings.py
@@ -70,6 +70,27 @@ EMAIL_USE_SSL = True
 EMAIL_USE_TLS = False
 
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "WARNING",
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": os.getenv("DJANGO_LOG_LEVEL", "INFO"),
+            "propagate": False,
+        },
+    },
+}
+
 WSGI_APPLICATION = "main.wsgi.application"
 
 
