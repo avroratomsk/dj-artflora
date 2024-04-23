cebc6dbad5cedcb66933063427304a6ba63514c2
sanchyopas <saniagolovanev@gmail.com>
2024-04-23 22:43:32 +0700

    FIX

--- main/main/settings.py
+++ main/main/settings.py
@@ -70,7 +70,36 @@ EMAIL_USE_SSL = True
 EMAIL_USE_TLS = False
 
 
-
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'console': {
+            'format': '%(name)-12s %(levelname)-8s %(message)s'
+        },
+        'file': {
+            'format': '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'console'
+        },
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'formatter': 'file',
+            'filename': '/home/a/avroraweb/irk-artlora.ru/public_html/debug.log'
+        }
+    },
+    'loggers': {
+        '': {
+            'level': 'INFO',
+            'handlers': ['console', 'file']
+        }
+    }
+}
 
 WSGI_APPLICATION = "main.wsgi.application"
 
