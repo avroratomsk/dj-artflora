7bffecedf38ed043ff71a4fa67256c942155ec59
sanchyopas <saniagolovanev@gmail.com>
2024-05-15 18:35:16 +0700

    FIX

--- main/order/email_send.py
+++ main/order/email_send.py
@@ -7,7 +7,7 @@ def email_send(order):
   html_content = render_to_string("mail/order_mail.html", {"order": order})
   from_email = "info@xn----7sbah6bllcobpj.xn--p1ai"
   text_content = 'Не поддерживает HTML в письме'
-  to = ['musik665@mail.ru']
+  to = ['saniagolovanev@gmai.com']
   msg = EmailMultiAlternatives(subject, text_content, from_email, to)
   msg.attach_alternative(html_content, "text/html")
   try:
